9ef3431f3157bf07133a68a065d09cfab039980e
J. Rook <jrook94@gmail.com>
2022-04-01 20:40:39 +0200

    Support for new reference data

--- resources/algorithms/_shared/utils.r
+++ resources/algorithms/_shared/utils.r
@@ -13,24 +13,12 @@ parse_instance_file <- function(filename,budget=1000){
 }
 
 process_run <- function(populations, fn, opt){
-    measures <- compute_performance_metrics(df, obj.fn, opt$instance)
+    measures <- compute_performance_metrics(df, obj.fn, opt)
 
     if (!is.null(opt$save_solution)){
-        writeLines("Save to file")
-        saveRDS(measures, file=opt$save_solution)
+        writeLines("Save to file", opt$save_solution)
+        saveRDS(as.data.frame(measures), file=opt$save_solution)
     }
-
-    #Depricated
-    # if(!is.null(opt$visualise)){
-    #     output <- opt$visualise
-    #     pdf(output)
-    #     plot(t(population), main="Decision space")
-    #     plot(t(solution_set), main="Objective space")
-    #     plot(t(pareto.matrix), main="Non-dominated set in objective space")
-    #     dev.off()
-    #     output <- paste0(opt$visualise,".Rdata")
-    #     save(populationnd, pareto.matrix, file=output)
-    # }
 
     print_measures(measures)
 }
@@ -48,13 +36,14 @@ process_run <- function(populations, fn, opt){
 # populations: a Tibble with fun_calls, decision space vector, obective space vector
 # fn: smoof function
 # instance_path: string of instance path
-compute_performance_metrics <- function (populations, fn, instance_path){
+compute_performance_metrics <- function (populations, fn, opt){
     #Get reference data
+    instance_path = opt$instance
     instance_name <- tail(unlist(strsplit(instance_path,"/")), n=1)
     load("refdata.RData")
     reference.point <- get_reference_point(fn, instance_path)
-    refoffset <- references[[instance_name]]$newoffset
-    besthv <- references[[instance_name]]$newhv
+    #refoffset <- references[[instance_name]]$newoffset
+    besthv <- references[[instance_name]]$hv
 
     #Destil last population from tibble
     last_pop <- populations %>% filter(max(populations$fun_calls) == populations$fun_calls)
@@ -70,10 +59,10 @@ compute_performance_metrics <- function (populations, fn, instance_path){
     if(is.matrix(solution_set)){
         #Limit for MOGSA and MOLE to prevent memory issues
         if(ncol(solution_set) > 2000) solution_set <- solution_set[,sample(ncol(solution_set), 2000)]
-        measures$HV <- -ecr3vis::hv(solution_set + refoffset, reference.point)
+        measures$HV <- -ecr3vis::hv(solution_set, reference.point)
     }
     else if(sum(reference.point < solution_set) == length(reference.point)){
-        measures$HV <- -prod(reference.point - (solution_set+refoffset))
+        measures$HV <- -prod(reference.point - (solution_set))
     }
     else {
         measures$HV <- 0
@@ -94,9 +83,10 @@ compute_performance_metrics <- function (populations, fn, instance_path){
     #TODO: IDEA: Make also a tibble of the smoof logging wrapper and use that to compute the ABSE measures
 
     #TODO Jonathan: basins levels sorted on HV
-    cat("ABSE ITERATIVE \n")
+    cat("ABSE ITERATIVE! \n")
+    unwrapped.fn = smoof::getWrappedFunction(fn)
     abse <- ABSE::evalutate_results(populations,
-                                    fn,
+                                    unwrapped.fn,
                                     ref.point=reference.point,
                                     basins = 1:4,
                                     keep.points=TRUE,
@@ -107,7 +97,7 @@ compute_performance_metrics <- function (populations, fn, instance_path){
 
     cat("ABSE CUMULATIVE \n")
     absec <- ABSE::evalutate_results(populations,
-                                    fn,
+                                    unwrapped.fn,
                                     ref.point=reference.point,
                                     basins = 1:4,
                                     keep.points=TRUE,
@@ -120,20 +110,20 @@ compute_performance_metrics <- function (populations, fn, instance_path){
     measures$ABSE.CUM.HV.AUC.B1 <- -tail(absec$basin_separated_eval$auc_hv1, n=1)
 
     cat("ABSE JF ITERATIVE \n")
-    abse <- ABSE::evalutate_results(populations,
-                                    fn,
+    absej <- ABSE::evalutate_results(populations,
+                                    unwrapped.fn,
                                     ref.point=reference.point,
                                     basins = 1:4,
                                     keep.points=TRUE,
                                     join_fronts=FALSE,
                                     design=abse)
-    measures$ABSE.JF.HV.MEAN <- -tail(abse$basin_separated_eval$mean_value, n=1)
-    measures$ABSE.JF.HV.AUC.MEAN <- -tail(abse$basin_separated_eval$auc_hv_mean, n=1)
-    measures$ABSE.JF.HV.AUC.B1 <- -tail(abse$basin_separated_eval$auc_hv1, n=1)
+    measures$ABSE.JF.HV.MEAN <- -tail(absej$basin_separated_eval$mean_value, n=1)
+    measures$ABSE.JF.HV.AUC.MEAN <- -tail(absej$basin_separated_eval$auc_hv_mean, n=1)
+    measures$ABSE.JF.HV.AUC.B1 <- -tail(absej$basin_separated_eval$auc_hv1, n=1)
 
     cat("ABSE JF CUMULATIVE \n")
-    absec <- ABSE::evalutate_results(populations,
-                                    fn,
+    absejc <- ABSE::evalutate_results(populations,
+                                    unwrapped.fn,
                                     ref.point=reference.point,
                                     basins = 1:4,
                                     keep.points=TRUE,
@@ -141,14 +131,26 @@ compute_performance_metrics <- function (populations, fn, instance_path){
                                     design=abse,
                                     efficient_sets = abse$efficientSets,
                                     dec_space_labels = abse$decSpaceLabels)
-    measures$ABSE.JF.CUM.HV.MEAN <- -tail(absec$basin_separated_eval$mean_value, n=1)
-    measures$ABSE.JF.CUM.HV.AUC.MEAN <- -tail(absec$basin_separated_eval$auc_hv_mean, n=1)
-    measures$ABSE.JF.CUM.HV.AUC.B1 <- -tail(absec$basin_separated_eval$auc_hv1, n=1)
+    measures$ABSE.JF.CUM.HV.MEAN <- -tail(absejc$basin_separated_eval$mean_value, n=1)
+    measures$ABSE.JF.CUM.HV.AUC.MEAN <- -tail(absejc$basin_separated_eval$auc_hv_mean, n=1)
+    measures$ABSE.JF.CUM.HV.AUC.B1 <- -tail(absejc$basin_separated_eval$auc_hv1, n=1)
 
     #AUC of trajectory
     #AUC of cummulative population generations#TODO Jonathan:  in ABSE package as option
     #ABSE score of all function calls combined (From smoof logger = fn)
     #ABSE score for last population
+
+
+    if(!is.null(opt$visualise)){
+        output <- opt$visualise
+        # pdf(output)
+        # plot(t(population), main="Decision space")
+        # plot(t(solution_set), main="Objective space")
+        # plot(t(pareto.matrix), main="Non-dominated set in objective space")
+        # dev.off()
+        output <- paste0(opt$visualise,".Rdata")
+        save(abse, absec, absej, absejc, file=output)
+    }
 
     return(measures)
 }
