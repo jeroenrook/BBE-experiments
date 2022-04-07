b6b69bcc0e386365ed9cfc801cdfeda4ae20f0a8
J. Rook <jrook94@gmail.com>
2022-04-07 12:59:50 +0200

    Configuration results

--- experiments/04configurationfast/do_validation.py
+++ experiments/04configurationfast/do_validation.py
@@ -13,51 +13,58 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--solver', default=None, type=str, help='Solver name')
     parser.add_argument('--instance', default=None, type=str, help='instance name')
-    parser.add_argument('--target', default="default", type=str, help='Target')
+    parser.add_argument('--target', default=None, type=str, help='Target')
     args = parser.parse_args()
 
     EXP_NAME = os.path.basename(os.getcwd())
     # Make scratch directory
     EXP_DIR = f"/scratch/tmp/rookj/tmp/ABSE/{EXP_NAME}"
 
-    target = args.target if args.target in ["default", "HVN", "SP", "ABSEHVMEANNORM", "ABSEHVAUCMEANNORM", "ABSECUMHVMEANNORM", "ABSECUMHVAUCMEANNORM"] else "default"
-    targetdirname = "HVN" if target == "default" else target
+    targets = ["default", "HVN", "SP", "ABSEHVMEANNORM", "ABSEHVAUCMEANNORM", "ABSECUMHVMEANNORM", "ABSECUMHVAUCMEANNORM"]
+    if args.target is not None:
+        target = args.target if args.target in targets else "default"
+        targets = [target]
 
-    solver_dir = f"{EXP_DIR}/{targetdirname}/sparkle/Solvers"
-    instance_dir = f"{EXP_DIR}/{targetdirname}/sparkle/Instances/instances"
+    for target in targets:
+        targetdirname = "HVN" if target == "default" else target
 
-    if args.solver is None:
-        solvers = os.listdir(solver_dir)
-    else:
-        solvers = [args.solver]
+        solver_dir = f"{EXP_DIR}/{targetdirname}/sparkle/Solvers"
+        instance_dir = f"{EXP_DIR}/{targetdirname}/sparkle/Instances/instances"
 
-    if args.instance is None:
-        instances = os.listdir(instance_dir)
-    else:
-        instances = [args.instance]
+        if args.solver is None:
+            solvers = os.listdir(solver_dir)
+        else:
+            solvers = [args.solver]
 
-    output_dir = f"{EXP_DIR}/validation"
-    os.system(f"mkdir {output_dir}")
+        if args.instance is None:
+            instances = os.listdir(instance_dir)
+        else:
+            instances = [args.instance]
 
-    for solver in solvers:
-        configarg = ""
-        if target != "default":
-            # Retrieve configuration
-            print("GET CONFIG")
-            config_path = f"{EXP_DIR}/{targetdirname}/sparkle/Components/smac-v2.10.03-master-778/example_scenarios/{solver}_instances/configuration_for_validation.txt"
-            if(os.path.isfile(config_path)):
-                config = open(config_path, "r").read().strip()
-                configarg = f"--configuration \"{config}\""
-            else:
-                print(f"No configuration found for this target. Skipping {solver}")
-                continue
+        output_dir = f"{EXP_DIR}/validation"
+        os.system(f"mkdir {output_dir}")
 
-        for instance in instances:
-            print(f"{solver} - {instance}")
-            solver_path = os.path.join(solver_dir, solver)
-            instance_path = os.path.join(instance_dir, instance)
-            output_name = f"{target}_{solver}_{instance}.pickle"
-            output_path = os.path.join(output_dir, output_name)
-            command = f"sbatch ../02validation/validation.py --instance {instance_path} --solver {solver_path} --runs 25 --output {output_path} {configarg}"
-            print(command)
-            os.system(command)
+        for solver in solvers:
+            configarg = ""
+            if target != "default":
+                # Retrieve configuration
+                print("GET CONFIG")
+                config_path = f"{EXP_DIR}/{targetdirname}/sparkle/Components/smac-v2.10.03-master-778/example_scenarios/{solver}_instances/configuration_for_validation.txt"
+                if(os.path.isfile(config_path)):
+                    config = open(config_path, "r").read().strip()
+                    configarg = f"--configuration \"{config}\""
+                else:
+                    print(f"No configuration found for this target. Skipping {solver}")
+                    continue
+
+            for instance in instances:
+                print(f"{solver} - {instance}")
+                solver_path = os.path.join(solver_dir, solver)
+                instance_path = os.path.join(instance_dir, instance)
+                output_name = f"{target}_{solver}_{instance}.pickle"
+                output_path = os.path.join(output_dir, output_name+".pickle")
+
+                visualisation_path = os.path.join("visualisation", output_name)
+                command = f"sbatch ../02validation/validation.py --instance {instance_path} --solver {solver_path} --runs 5 --budget 20000 --output {output_path} --visualise {visualisation_path} {configarg}"
+                print(command)
+                os.system(command)
